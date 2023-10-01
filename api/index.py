50aeda565b2c9222a9a2087f7487d371983c607f
likun <630662717@qq.com>
2023-10-01 22:26:58 +0800

    ssadasd

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
 from http.server import BaseHTTPRequestHandler
 
+import flask
+from flask import Flask
+
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        name = flask.request.args.get('name')
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(" aaaaaaaaa   ".encode())
+        self.wfile.write(name + " aaaaaaaaa   ".encode())
         return
