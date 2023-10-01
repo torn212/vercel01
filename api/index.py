5b0977b170e3c723cd076011f04e8233e44c3549
likun <630662717@qq.com>
2023-10-01 22:29:40 +0800

    ssadasd

--- api/index.py
+++ api/index.py
@@ -1,15 +1,8 @@
-from http.server import BaseHTTPRequestHandler
-
-import flask
 from flask import Flask
 
+app = Flask(__name__)
 
-class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        name = flask.request.args.get('name')
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(name + " aaaaaaaaa   ".encode())
-        return
+@app.route('/')
+def home():
+    return 'Hello, World!'
